aa376de020074340b8e7817364e741da62f7de76
LuckepuckeR <lucas.runden@hotmail.se>
2025-09-17 16:55:01 +0200

    printar alla bokstäver för sig men hoppar över mellanslag

--- Word_Counting/wordfreq.py
+++ Word_Counting/wordfreq.py
@@ -1,5 +1,20 @@
 
 
 def tokenize(lines):
-    pass
+    words = []
+    for line in lines:
+        start = 0
+
+        while start < len(line):
+
+            while start < len(line) and line[start].isspace():
+                start = start+1
+            
+            if start >= len(line):
+                break
+
+            print(line[start])
+
+            start = start+1
+    return words
 
