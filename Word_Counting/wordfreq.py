1b5768f821779446736667a46b423abee2c7317c
LuckepuckeR <lucas.runden@hotmail.se>
2025-09-17 17:51:12 +0200

    färdig funktion

--- Word_Counting/wordfreq.py
+++ Word_Counting/wordfreq.py
@@ -9,12 +9,42 @@ def tokenize(lines):
 
             while start < len(line) and line[start].isspace():
                 start = start+1
-            
+
             if start >= len(line):
                 break
 
-            print(line[start])
 
-            start = start+1
+            end = start
+
+
+            while end < len(line) and line[end].isalpha():
+                end = end+1
+            
+            if end > start:
+                word = line[start:end]
+                lower = word.lower()
+                words.append(lower)
+                start = end
+                continue
+
+            while end < len(line) and line[end].isdigit():
+                end = end+1
+            
+            if end > start:
+                words.append(line[start:end])
+                start = end
+                continue
+
+
+            while end < len(line) and not line[end].isdigit() and not line[end].isalpha() and not line[end].isspace():
+                end = end+1
+
+            if end > start:
+                words.append(line[start:end])
+                start = end
+                continue
+
+
+            start = end
+
     return words
-
