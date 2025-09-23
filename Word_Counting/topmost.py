36e8cd7ec5554611333521c8046d49893755524b
LuckepuckeR <lucas.runden@hotmail.se>
2025-09-23 14:20:59 +0200

    Klar

--- Word_Counting/topmost.py
+++ Word_Counting/topmost.py
@@ -3,41 +3,50 @@ import sys
 import urllib.request
 
 stopwords = [
-    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves',
-    'you', "you're", "you've", "you'll", "you'd", 'your', 'yours',
-    'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she',
-    "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself',
-    'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which',
-    'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am',
-    'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has',
-    'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the',
-    'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of',
-    'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
-    'through', 'during', 'before', 'after', 'above', 'below', 'to',
-    'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under',
-    'again', 'further', 'then', 'once', 'here', 'there', 'when',
-    'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few',
-    'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not',
-    'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't',
-    'can', 'will', 'just', 'don', "don't", 'should', "should've",
-    'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren',
-    "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn',
-    "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven',
-    "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't",
-    'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't",
-    'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't",
-    'won', "won't", 'wouldn', "wouldn't"]
+    ".", "?", "!", ",", "(", ")", "[", "]", "{", "}", "-", "–", "_", '"', "'", "’", "‘", "·", ":", ";",
+    "$", "£", "€", "%", "#", "@", "\\", "+", "^", "/", "=", "±", "&", "—", "*",
+    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "the", "a", "an", "and", "or", "nor", "just", "never", "quite",
+    "am", "is", "are", "was", "were", "be", "been",
+    "do", "did", "does",
+    "may", "might", "can", "must", "should", "shall", "could", "would",
+    "to", "of", "in", "for", "as", "on", "off", "by", "at", "but", "such", "also",
+    "this", "these", "those", "that", "here", "there", "how", "where", "when",
+    "no", "upon", "out", "up", "down", "into",
+    "i", "me", "my", "myself",
+    "you", "your", "yourself",
+    "he", "his", "him", "himself",
+    "she", "her", "herself",
+    "it", "its", "itself",
+    "we", "our", "us",
+    "they", "their", "them", "themselves",
+    "s", "t", "d", "n", "o", "with",
+    "all", "not", "from", "so", "now",
+    "which", "whose", "like", "self", "most", "one", "once", "first", "two", "twice", "second", "three", "last",
+    "have", "has", "had", "then",
+    "what", "some", "more", "if", "other", "only", "yet",
+    "will", "over", "any", "who", "though", "than", "whom", "very", "about", "still", "before", "after", "again", "while", "away",
+    "go", "went", "goes", "come", "comes", "came",
+    "say", "says", "said",
+    "see", "sees", "saw",
+    "let", "give", "gives", "gave",
+    "take", "takes", "took",
+    "nothing", "something", "gutenberg", "project"]
 
 def main():
-    filename = "C:\\Users\\Administratör\\Desktop\\DAT425\\Word_Counting\\article1.txt"
+    #filename = "C:\\Users\\Administratör\\Desktop\\DAT425\\Word_Counting\\examples\\article7.txt"
+    link = "https://www.gutenberg.org/files/15/15-0.txt"
 
-    inp_file = open(filename, encoding="utf-8")
-    lines = inp_file.readlines()
-    inp_file.close()
+    #inp_file = open(filename, encoding="utf-8")
+    #lines = inp_file.readlines()
+    #inp_file.close()
+
+    response = urllib.request.urlopen(link)
+    lines = response.read().decode("utf-8").splitlines()
 
     token = wordfreq.tokenize(lines)
     frequencies = wordfreq.countWords(token, stopwords)
-    return wordfreq.printTopMost(frequencies, 5)
+    return wordfreq.printTopMost(frequencies, 10)
 
 
 main()
